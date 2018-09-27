f214c5b99215f5dc6965c91ed5a470fb4cd95c5c
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2018-09-27 16:40:14 -0400

    update

--- check_if_html5.sh
+++ check_if_html5.sh
@@ -3,4 +3,14 @@ set -ev
 
 if [[ $(git diff --name-only HEAD..testing-base) = *"bigbluebutton-html5"* ]]; then
   echo "HTML5"
+  cd bigbluebutton-html5
+  curl https://install.meteor.com/ | sh
+  meteor npm install
+  cd tests/puppeteer
+  npm install
+  cd ../../..
+  FILES=`git diff --name-only HEAD..$TRAVIS_BRANCH`
+  bigbluebutton-html5/node_modules/.bin/eslint --ext .jsx,.js $(git diff --name-only HEAD..testing-base)
+else
+  echo "not HTML5"
 fi

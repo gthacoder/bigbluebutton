2d97c73692d03a69234ebfb502a8333105bcfea7
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2018-10-01 16:37:53 -0400

    update

--- check_if_html5.sh
+++ check_if_html5.sh
@@ -3,7 +3,18 @@ set -ev
 
 if [[ $(git diff --name-only HEAD..testing-base) = *"bigbluebutton-html5"* ]]; then
   echo "HTML5"
-  cd bigbluebutton-html5
+
+  docker build -t b2 .
+  docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)
+  echo $docker
+  cd tests/puppeteer
+  npm install
+  conf=$(docker exec $(docker ps -q) bbb-conf --secret | grep "Secret:")
+  secret=$(echo $conf | cut -d' ' -f2)
+  export BBB_SHARED_SECRET=$secret
+
+  cd ../..
+  #cd bigbluebutton-html5
   curl https://install.meteor.com/ | sh
   meteor npm install
   cd tests/puppeteer

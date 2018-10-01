5ab83922133be06e8ca59fd609aa18f003f3f71e
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2018-10-01 17:35:27 -0400

    update

--- check_if_html5.sh
+++ check_if_html5.sh
@@ -5,7 +5,7 @@ if [[ $(git diff --name-only HEAD..testing-base) = *"bigbluebutton-html5"* ]]; t
   echo "HTML5"
 
   cd bigbluebutton-html5
-  docker build -t b2 .
+  docker build -t b2 -f Dockerfile.test .
   docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)
   echo $docker
   cd tests/puppeteer

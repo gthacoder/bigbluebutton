f8db0ae049baea2d83134350a310328841cc998e
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2018-10-02 15:53:00 -0400

    update

--- build_script.sh
+++ build_script.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -ev
 
-if [[ $(git diff --name-only HEAD..testing-base) = *"bigbluebutton-html5"* ]]; then
-  echo "HTML5"
-
+FILES=`git diff --name-only HEAD..$TRAVIS_BRANCH`
+if [[ $files = *"bigbluebutton-html5"* ]]; then
   cd bigbluebutton-html5
   docker build -t b2 -f Dockerfile.test .
   docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)
@@ -20,10 +19,5 @@ if [[ $(git diff --name-only HEAD..testing-base) = *"bigbluebutton-html5"* ]]; t
   cd tests/puppeteer
   npm install
   cd ../../..
-  FILES=`git diff --name-only HEAD..$TRAVIS_BRANCH`
-  echo "FILES:"
-  echo $FILES
-  bigbluebutton-html5/node_modules/.bin/eslint --ext .jsx,.js $(git diff --name-only HEAD..testing-base)
-else
-  echo "not HTML5"
+  bigbluebutton-html5/node_modules/.bin/eslint --ext .jsx,.js $files
 fi

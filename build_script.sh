9f03628a86b7ec41ad62be83bb2b15ae8cb63fca
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2018-10-02 15:25:16 -0400

    update

--- build_script.sh
+++ build_script.sh
@@ -15,13 +15,14 @@ if [[ $(git diff --name-only HEAD..testing-base) = *"bigbluebutton-html5"* ]]; t
   export BBB_SHARED_SECRET=$secret
 
   cd ../..
-  #cd bigbluebutton-html5
   curl https://install.meteor.com/ | sh
   meteor npm install
   cd tests/puppeteer
   npm install
   cd ../../..
   FILES=`git diff --name-only HEAD..$TRAVIS_BRANCH`
+  echo "FILES:"
+  echo $FILES
   bigbluebutton-html5/node_modules/.bin/eslint --ext .jsx,.js $(git diff --name-only HEAD..testing-base)
 else
   echo "not HTML5"

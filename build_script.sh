493dc1b313e228fde1d4df0d246d6e8df2f7c58d
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2018-10-04 14:44:27 -0400

    update

--- build_script.sh
+++ build_script.sh
@@ -6,16 +6,19 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
   cd bigbluebutton-html5
   echo "cloning:"
   git clone https://github.com/bigbluebutton/docker.git
-  echo "ls:"
-  ls docker
-  echo "copying setup.sh:"
-  git archive --remote=git@github.com:bigbluebutton/docker.git HEAD setup.sh | tar -x
-  echo "ls:"
+  echo "ls here:"
   ls
-  docker build -t b2 -f Dockerfile.test .
+  echo "ls there:"
+  ls docker
+  rm docker/Dockerfile
+  cp Dockerfile.test docker/Dockerfile
+  cd docker
+  docker build -t b2 .
+  #docker build -t b2 -f Dockerfile.test .
   docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)
   echo $docker
-  cd tests/puppeteer
+  cd ../test/puppeteer
+  #cd tests/puppeteer
   npm install
   conf=$(docker exec $(docker ps -q) bbb-conf --secret | grep "Secret:")
   secret=$(echo $conf | cut -d' ' -f2)

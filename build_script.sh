d5f0cb049f972fa62badc14b906c8e4f16e21a3d
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2018-11-19 17:47:57 -0500

    test

--- build_script.sh
+++ build_script.sh
@@ -10,7 +10,7 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
   then
     cd ..
     bigbluebutton-html5/node_modules/.bin/eslint --ext .jsx,.js $files
-  else
+  elif [ $1 = acceptance_tests ]
     {
       git clone --single-branch -b update-html5 https://github.com/bigbluebutton/docker.git
       cp -r docker/{mod,restart.sh,setup.sh,supervisord.conf} .
@@ -30,33 +30,3 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
     npm test
   fi
 fi
-#  {
-#    cd bigbluebutton-html5
-#    git clone --single-branch -b update-html5 https://github.com/bigbluebutton/docker.git
-#    cp -r docker/{mod,restart.sh,setup.sh,supervisord.conf} .
-#    cp -r docker/Dockerfile Dockerfile.test
-#    docker build -t b2 -f Dockerfile.test .
-#    docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)
-#    echo $docker
-#  } > /dev/null
-
-#  cd tests/puppeteer
-#  npm install
-#  conf=$(docker exec $(docker ps -q) bbb-conf --secret | grep "Secret:")
-#  secret=$(echo $conf | cut -d' ' -f2)
-#  export BBB_SHARED_SECRET=$secret
-#  cd ../..
-#  curl https://install.meteor.com/ | sh
-#  meteor npm install
-
-#  if [ $1 = linter ]
-#  then
-#    node_modules/.bin/eslint --ext .jsx,.js $files
-#  else
-#    cd tests/puppeteer
-#    npm install
-#    node html5-check.js
-#    npm test
-#  fi
-#fi
-

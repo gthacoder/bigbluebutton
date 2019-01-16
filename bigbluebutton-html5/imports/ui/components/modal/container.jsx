183c7b3e7307eb5e94019f484ddeb5b137e21a68
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-01-16 11:43:04 -0500

    test

--- bigbluebutton-html5/imports/ui/components/modal/container.jsx
+++ bigbluebutton-html5/imports/ui/components/modal/container.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { getModal } from './service';
 
+// test
+
 export default withTracker(() => ({ modalComponent: getModal() }))(({ modalComponent }) => modalComponent);

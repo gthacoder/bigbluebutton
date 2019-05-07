023128e17c833dd8352f27030d4c02139f4eb335
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-05-07 18:23:42 +0000

    test

--- build_script.sh
+++ build_script.sh
@@ -22,16 +22,16 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
     bigbluebutton-html5/node_modules/.bin/eslint --ext .jsx,.js $html5_files
   elif [ $1 = acceptance_tests ]
   then
-    {
-      git clone --single-branch -b update-html5 https://github.com/bigbluebutton/docker.git
-      cp -r docker/{mod,setup.sh,supervisord.conf} .
-      cp -r docker/Dockerfile Dockerfile.test
-      docker build -t b2 -f Dockerfile.test .
-      docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)
-      echo $docker
-    } > /dev/null
+    git clone --single-branch -b update-html5 https://github.com/bigbluebutton/docker.git
+    cp -r docker/{mod,setup.sh,supervisord.conf} .
+    cp -r docker/Dockerfile Dockerfile.test
+    echo "here 1"
+    docker build -t b2 -f Dockerfile.test .
+    echo "here 2"
+    docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)
+    echo $docker
 
-    echo "here"
+    echo "here 3"
 
     cd tests/puppeteer/core
     conf=$(docker exec $(docker ps -q) bbb-conf --secret | grep "Secret:")

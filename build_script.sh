635b94ab283e98b480e5c7a82fc25c13d4553d8d
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-05-08 15:43:10 +0000

    test

--- build_script.sh
+++ build_script.sh
@@ -22,26 +22,24 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
     bigbluebutton-html5/node_modules/.bin/eslint --ext .jsx,.js $html5_files
   elif [ $1 = acceptance_tests ]
   then
-    git clone --single-branch -b update-html5 https://github.com/bigbluebutton/docker.git
-    cp -r docker/{mod,setup.sh,supervisord.conf} .
-    cp -r docker/Dockerfile Dockerfile.test
-    echo "here 1"
-    docker build -t b2 -f Dockerfile.test .
-    echo "here 2"
-    docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)
-    echo $docker
+    #git clone --single-branch -b update-html5 https://github.com/bigbluebutton/docker.git
+    #cp -r docker/{mod,setup.sh,supervisord.conf} .
+    #cp -r docker/Dockerfile Dockerfile.test
+    #docker build -t b2 -f Dockerfile.test .
+    #docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)
+    #echo $docker
+    #cd bigbluebutton-html5/tests/puppeteer/core
+    #conf=$(docker exec $(docker ps -q) bbb-conf --secret | grep "Secret:")
 
-    echo "here 3"
+    cd bigbluebutton-html5
+    npm install
+    npm start &
+    cd tests/puppeteer/core
+    conf=$(bbb-conf --secret | grep "Secret:")
 
-    ls
-    echo "here 4"
-    cd bigbluebutton-html5/tests/puppeteer/core
-    conf=$(docker exec $(docker ps -q) bbb-conf --secret | grep "Secret:")
     secret=$(echo $conf | cut -d' ' -f2)
     export BBB_SHARED_SECRET=$secret
     node html5-check.js
-    #cd ../../..
-    #npm test
     docker stop $docker
     docker rm $docker
     docker rmi b2

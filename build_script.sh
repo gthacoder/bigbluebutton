d32ddeb3895bbec41e7f9ff75154b0557f1511e1
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-09-19 14:05:22 -0400

    test

--- build_script.sh
+++ build_script.sh
@@ -24,7 +24,7 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
   then
     {
       git clone --single-branch -b update-html5 https://github.com/bigbluebutton/docker.git
-      cp -r docker/{mod,restart.sh,setup.sh,supervisord.conf} .
+      cp -r docker/{mod,setup.sh,supervisord.conf} .
       cp -r docker/Dockerfile Dockerfile.test
       docker build -t b2 -f Dockerfile.test .
       docker=$(docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp b2 -h localhost)

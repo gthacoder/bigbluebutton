24100e9d33d7fa1cf705e09eb76506f6f8642d37
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-09-25 14:13:19 -0400

    test

--- build_script.sh
+++ build_script.sh
@@ -60,7 +60,7 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
 
     #sudo apt-get install xvfb
 
-    #cd bigbluebutton-html5
+    cd bigbluebutton-html5
     #./node_modules/.bin/webdriver-manager update --gecko=false --versions.chrome=v77.0.3865.90-1
     #xvfb-run ./node_modules/.bin/webdriver-manager start --versions.chrome=v77.0.3865.90-1 &
     #sleep 5

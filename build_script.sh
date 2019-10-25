b9a612c361db971edd0bfbaf2961927f0fc77515
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-10-25 15:01:14 -0400

    test

--- build_script.sh
+++ build_script.sh
@@ -25,9 +25,7 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
     git clone --single-branch -b bbb-22 https://github.com/MaximKhlobystov/docker.git
     cp -r docker/{mod,setup.sh,supervisord.conf} .
     cp -r docker/Dockerfile Dockerfile.test
-    docker login -u "$REGISTRY_USER" -p "$REGISTRY_PASS"
-    docker pull maximkhlobystov/bbb
-    docker build --cache-from maximkhlobystov/bbb -f Dockerfile.test .
+    docker build -t bbb -f Dockerfile.test .
     #docker build -t bbb -f Dockerfile.test .
     #docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp bbb -h localhost
     docker run --privileged -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp bbb -h localhost
@@ -48,7 +46,7 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
     #cd ../../..
     #ls
 
-    docker --version
+    #docker --version
 
     container=$(docker ps -q)
     echo $container
@@ -63,7 +61,7 @@ if [[ $files = *"bigbluebutton-html5"* ]]; then
     #echo "before start"
     #docker exec $container bash
     docker exec $container supervisorctl status
-    docker exec $container cat /var/log/supervisord.log
+    #docker exec $container cat /var/log/supervisord.log
     #docker exec $container service supervisor start
     #service supervisor start
     #echo "after start"

73097cb0ff157764daeb2d4a32741662b63a85ba
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-11-07 15:46:13 -0500

    test

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -3,9 +3,12 @@ git clone --single-branch -b bbb-22 https://github.com/MaximKhlobystov/docker.gi
 cp -r docker/{mod,setup.sh,supervisord.conf} .
 cp -r docker/Dockerfile Dockerfile.test
 docker build -t bbb -f Dockerfile.test .
-docker run --privileged -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp bbb -h localhost
+docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp bbb -h localhost
 container=$(docker ps -q)
 echo $container
+docker ps --all
+sleep 10
+docker ps --all
 #docker stop $container
 #docker start $container
 #docker exec $container supervisorctl status bbb-html5

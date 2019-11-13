6b4b588a09afc832a8ece253332dddc81bd5485f
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-11-13 16:16:52 -0500

    test

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -7,8 +7,6 @@ docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478
 container=$(docker ps -q)
 echo $container
 docker ps --all
-sleep 30
-docker ps --all
 sleep 60
 docker ps --all
 #docker exec $container service supervisor status
@@ -26,9 +24,9 @@ docker exec $container echo $CIRCLE_SHA1
 #docker exec $container service supervisor start
 #docker ps --all
 #docker start $container
-docker ps --all
 
-docker exec $container ./inside_script.sh
+ls
+docker exec $container .circleci/inside_script.sh
 
 #docker exec $container service supervisor status
 #docker exec $container supervisorctl status bbb-html5

5328428fe8c1eabb3ea0c9fb18c51a17306c5684
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-11-21 14:41:23 -0500

    test

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -30,7 +30,8 @@ echo $CIRCLE_WORKING_DIRECTORY
 docker exec $container ls
 docker exec $container git clone https://github.com/MaximKhlobystov/bigbluebutton.git
 docker exec $container ls
-docker exec $container bash -c "cd bigbluebutton && npm install"
+docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && npm install"
+docker exec $container supervisorctl status bbb-html5
 #docker exec -it $container /bin/bash
 
 #docker exec $container service supervisor status

5e5d019f1d815dc51353b629f05b35e8766ee0f8
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-11-22 17:00:54 -0500

    test

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -35,8 +35,11 @@ docker exec $container supervisorctl status bbb-html5
 docker exec $container supervisorctl stop bbb-html5
 docker exec $container supervisorctl status bbb-html5
 docker exec $container bash -c "curl https://install.meteor.com/ | sh"
-docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && npm install forever -g && ROOT_URL=http://127.0.0.1/html5client NODE_ENV=development METEOR_ALLOW_SUPERUSER=true forever meteor"
+#docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && npm install forever -g && ROOT_URL=http://127.0.0.1/html5client NODE_ENV=development METEOR_ALLOW_SUPERUSER=true forever meteor"
 #docker exec -it $container /bin/bash
+
+docker exec -it $container bash
+echo "test"
 
 sleep 60s
 

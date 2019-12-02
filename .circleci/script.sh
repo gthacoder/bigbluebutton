0b56d2934e407f8f9b99a04229451d12587f0cb0
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-12-02 15:19:45 -0500

    test

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -7,22 +7,22 @@ docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478
 container=$(docker ps -q)
 echo $container
 docker ps --all
-sleep 10
-docker ps --all
-docker exec $container service supervisor status
-docker exec $container ls
-docker exec $container echo $CIRCLE_REPOSITORY_URL
-docker exec $container echo $CIRCLE_SHA1
+#sleep 10
+#docker ps --all
+#docker exec $container service supervisor status
+#docker exec $container ls
+#docker exec $container echo $CIRCLE_REPOSITORY_URL
+#docker exec $container echo $CIRCLE_SHA1
 
-echo $CIRCLE_WORKING_DIRECTORY
+#echo $CIRCLE_WORKING_DIRECTORY
 
-docker exec $container ls
+#docker exec $container ls
 docker exec $container git clone https://github.com/MaximKhlobystov/bigbluebutton.git
-docker exec $container ls
+#docker exec $container ls
 docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && npm install"
-docker exec $container supervisorctl status bbb-html5
+#docker exec $container supervisorctl status bbb-html5
 docker exec $container supervisorctl stop bbb-html5
-docker exec $container supervisorctl status bbb-html5
+#docker exec $container supervisorctl status bbb-html5
 docker exec $container bash -c "curl https://install.meteor.com/ | sh"
 
 docker exec -it $container curl -I localhost/html5client/check
@@ -40,10 +40,8 @@ docker exec -it $container curl -I localhost/html5client/check
 
 echo "TESTING"
 
-docker exec $container bash -c "wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -"
-docker exec $container bash -c `sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'`
-docker exec $container bash -c "sudo apt-get update && sudo apt-get install -y google-chrome-stable"
-
+docker exec $container bash -c "wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - && echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list && sudo apt-get update"
+docker exec $container bash -c "sudo apt-get install google-chrome-stable"
 docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && npm test -- --spec ./tests/webdriverio/specs/chat.spec.js"
 
 echo "FINISHED"

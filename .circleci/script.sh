23fc220d7491a64966bbeffb590d14c33194894b
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-12-03 15:51:41 -0500

    test

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -44,6 +44,10 @@ docker exec -it $container curl -I localhost/html5client/check
 
 echo "TESTING"
 
-docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && npm test -- --spec ./tests/webdriverio/specs/chat.spec.js"
+docker exec $container bash -c "sudo apt-get install nodejs-legacy && cd bigbluebutton/bigbluebutton-html5/test/pptr && node test.js"
+
+#docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && npm test -- --spec ./tests/webdriverio/specs/chat.spec.js"
+
+
 
 echo "FINISHED"

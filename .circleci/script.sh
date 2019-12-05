bc06e5aa2d1355c1b2d4cd62f2d578241569f17b
Maxim Khlobystov <maxim.khlobystov@gmail.com>
2019-12-05 14:30:55 -0500

    test

--- .circleci/script.sh
+++ .circleci/script.sh
@@ -1,12 +1,9 @@
-ls
 git clone --single-branch -b bbb-22-1 https://github.com/MaximKhlobystov/docker.git
 cp -r docker/{mod,setup.sh,supervisord.conf} .
 cp -r docker/Dockerfile Dockerfile.test
 docker build -t bbb -f Dockerfile.test .
 docker run -d -p 80:80/tcp -p 443:443/tcp -p 1935:1935 -p 5066:5066 -p 3478:3478 -p 3478:3478/udp bbb -h localhost
 container=$(docker ps -q)
-echo $container
-docker ps --all
 docker exec $container echo $CIRCLE_PR_REPONAME
 docker exec $container echo $CIRCLE_PR_USERNAME
 docker exec $container echo $CIRCLE_SHA1
@@ -16,7 +13,7 @@ docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && npm inst
 docker exec $container supervisorctl stop bbb-html5
 docker exec $container bash -c "curl https://install.meteor.com/ | sh"
 
-docker exec -it $container curl -I localhost/html5client/check
+docker exec -it $container bash -c "curl -I localhost/html5client/check | head -n 1"
 
 docker exec $container bash -c "sudo apt-get install -y firefox && sudo apt-get install -y xvfb"
 docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && curl https://install.meteor.com/ | sh && ROOT_URL=http://127.0.0.1/html5client NODE_ENV=development METEOR_ALLOW_SUPERUSER=true meteor" &
@@ -25,14 +22,12 @@ echo "test"
 
 sleep 250s
 
-docker exec -it $container curl -I localhost/html5client/check
+docker exec -it $container bash -c "curl -I localhost/html5client/check | head -n 1"
 
 echo "TESTING"
 
 docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5/tests/pptr && npm install -g mocha && mocha test.js"
 
 #docker exec $container bash -c "cd bigbluebutton/bigbluebutton-html5 && npm test -- --spec ./tests/webdriverio/specs/chat.spec.js"
-
-
 
 echo "FINISHED"
